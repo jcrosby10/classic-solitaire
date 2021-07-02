01e1d8c5b2731113822d1c2de979ba27bf41c54e
Jason Crosby <jason.crosby2012@gmail.com>
2021-07-02 08:06:02 -0500

    more updates

--- app/src/main/java/com/huntergaming/classicsolitaire/ui/MainActivity.kt
+++ app/src/main/java/com/huntergaming/classicsolitaire/ui/MainActivity.kt
@@ -8,20 +8,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import com.huntergaming.classicsolitaire.R
-
 import com.huntergaming.classicsolitaire.ui.theme.ClassicSolitaireTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -42,8 +47,8 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun AppScreen(isLoggedIn: Boolean) {// debug are we going to the right screen
-    if (isLoggedIn) {
+fun AppScreen(isLoggedIn: Boolean) {
+    if (!isLoggedIn) {
         LoginScreen()
     } else {
        MainMenu()
@@ -56,7 +61,8 @@ fun MainMenu() {
 }
 
 @Composable
-fun LoginScreen(/*userInfo: UserInfo = UserInfo()*/) {
+fun LoginScreen() {
+    var passwordVisibility = remember { false }
     Text(
         text = stringResource(id = R.string.login_title),
         style = MaterialTheme.typography.h4,
@@ -71,36 +77,53 @@ fun LoginScreen(/*userInfo: UserInfo = UserInfo()*/) {
         verticalArrangement = Arrangement.Center
     ) {
         Row(modifier = Modifier.padding(dimensionResource(id = R.dimen.container_edge_padding))) {
+            val textState = remember { mutableStateOf(TextFieldValue()) }
             Text(
-                text = "${stringResource(id = R.string.email)} : ",
+                text = stringResource(id = R.string.login_email),
                 modifier = Modifier
                     .padding(dimensionResource(id = R.dimen.child_edge_padding)) // equivalent to padding inside
-                    .padding(end = dimensionResource(id = R.dimen.child_edge_padding)) // second padding acts as to margin putting space on the inside of the item
+                    .padding(end = dimensionResource(id = R.dimen.child_edge_padding)) // second padding acts as to margin putting space on the outside of the item
             )
             TextField(
-                value = "",
-                onValueChange = {  },
+                value = textState.value,
+                onValueChange = { textState.value = it },
                 modifier = Modifier
-                    .padding(dimensionResource(id = R.dimen.child_edge_padding))
-                    .padding(end = dimensionResource(id = R.dimen.child_edge_padding)),
+                    .padding(start = dimensionResource(id = R.dimen.child_edge_padding)),
                 placeholder =  { Text(text = stringResource(id = R.string.login_email_hint)) },
-                                                                     // var passwordVisibility by remember { mutableStateOf(false) }
-                visualTransformation = PasswordVisualTransformation()// if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation()
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation()
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.container_edge_padding))
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Checkbox(
+                checked = passwordVisibility,
+                onCheckedChange = { passwordVisibility = !passwordVisibility },
+                modifier = Modifier
+                    .padding(end = dimensionResource(id = R.dimen.child_edge_padding))
+            )
+            Text(
+                text = stringResource(id = R.string.login_show_password),
+                modifier = Modifier
+                    .padding(start = dimensionResource(id = R.dimen.child_edge_padding))
             )
         }
 
         Row(modifier = Modifier.padding(dimensionResource(id = R.dimen.container_edge_padding))) {
+            val textState = remember { mutableStateOf(TextFieldValue()) }
             Text(
-                text = "${stringResource(id = R.string.password)} : ",
+                text = stringResource(id = R.string.login_password),
                 modifier = Modifier
-                    .padding(dimensionResource(id = R.dimen.child_edge_padding))
                     .padding(end = dimensionResource(id = R.dimen.child_edge_padding))
             )
             TextField(
-                value = "",
-                onValueChange = { },
+                value = textState.value,
+                onValueChange = { textState.value = it },
                 modifier = Modifier
-                    .padding(dimensionResource(id = R.dimen.child_edge_padding))
                     .padding(start = dimensionResource(id = R.dimen.child_edge_padding)),
                 placeholder = { Text(text = stringResource(id = R.string.login_password_hint)) }
             )
@@ -117,7 +140,8 @@ fun LoginScreen(/*userInfo: UserInfo = UserInfo()*/) {
             )
             Button(
                 onClick = {  },
-                content = { Text(text = stringResource(id = R.string.create_account)) }
+                content = { Text(text = stringResource(id = R.string.create_account)) },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.child_edge_padding))
             )
         }
     }
@@ -127,6 +151,6 @@ fun LoginScreen(/*userInfo: UserInfo = UserInfo()*/) {
 @Composable
 private fun DefaultPreview() {
     ClassicSolitaireTheme {
-        LoginScreen()
+        AppScreen(isLoggedIn = false)
     }
 }
