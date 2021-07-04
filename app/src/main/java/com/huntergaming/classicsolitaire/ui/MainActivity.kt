2484072835b5b8bfa8c632c1ef5aa4e4a4df43a2
Jason <jason.crosby2012@gmail.com>
2021-07-04 09:46:54 -0500

    added typography

--- app/src/main/java/com/huntergaming/classicsolitaire/ui/MainActivity.kt
+++ app/src/main/java/com/huntergaming/classicsolitaire/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,10 +15,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Checkbox
+import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,7 +32,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.zIndex
 import com.huntergaming.classicsolitaire.R
 import com.huntergaming.classicsolitaire.ui.theme.ClassicSolitaireTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -40,6 +46,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -72,23 +80,36 @@ fun MainMenu() {
 @Composable
 fun LoginScreen() {
     var passwordVisibility = remember { false }
+
     Text(
-        text = stringResource(id = R.string.login_title),
-        style = MaterialTheme.typography.h4,
-        modifier = Modifier.padding(dimensionResource(id = R.dimen.container_edge_padding))
+        text = stringResource(id = R.string.app_name),
+        style = MaterialTheme.typography.h1,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(dimensionResource(id = R.dimen.container_edge_padding))
+            .fillMaxWidth()
+            .zIndex(1f)
     )
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(MaterialTheme.colors.background)
             .padding(dimensionResource(id = R.dimen.container_edge_padding)),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Text(
+            text = stringResource(id = R.string.login_title),
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.container_edge_padding))
+        )
+
         Row(modifier = Modifier.padding(dimensionResource(id = R.dimen.container_edge_padding))) {
             val textState = remember { mutableStateOf(TextFieldValue()) }
             Text(
                 text = stringResource(id = R.string.login_email),
+                style = MaterialTheme.typography.body1,
                 modifier = Modifier
                     .padding(dimensionResource(id = R.dimen.child_edge_padding)) // equivalent to padding inside
                     .padding(end = dimensionResource(id = R.dimen.child_edge_padding)) // second padding acts as to margin putting space on the outside of the item
@@ -96,9 +117,18 @@ fun LoginScreen() {
             TextField(
                 value = textState.value,
                 onValueChange = { textState.value = it },
-                modifier = Modifier
-                    .padding(start = dimensionResource(id = R.dimen.child_edge_padding)),
-                placeholder =  { Text(text = stringResource(id = R.string.login_email_hint)) },
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = MaterialTheme.colors.onPrimary,
+                    backgroundColor = MaterialTheme.colors.background,
+                    errorIndicatorColor = MaterialTheme.colors.error
+                ),
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.child_edge_padding)),
+                placeholder =  {
+                    Text(
+                        text = stringResource(id = R.string.login_email_hint),
+                        style = MaterialTheme.typography.body1
+                    )
+               },
                 visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation()
             )
         }
@@ -112,11 +142,17 @@ fun LoginScreen() {
             Checkbox(
                 checked = passwordVisibility,
                 onCheckedChange = { passwordVisibility = !passwordVisibility },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = MaterialTheme.colors.secondary,
+                    uncheckedColor = MaterialTheme.colors.primaryVariant,
+                    checkmarkColor = MaterialTheme.colors.onPrimary
+                ),
                 modifier = Modifier
                     .padding(end = dimensionResource(id = R.dimen.child_edge_padding))
             )
             Text(
                 text = stringResource(id = R.string.login_show_password),
+                style = MaterialTheme.typography.body1,
                 modifier = Modifier
                     .padding(start = dimensionResource(id = R.dimen.child_edge_padding))
             )
@@ -126,15 +162,25 @@ fun LoginScreen() {
             val textState = remember { mutableStateOf(TextFieldValue()) }
             Text(
                 text = stringResource(id = R.string.login_password),
+                style = MaterialTheme.typography.body1,
                 modifier = Modifier
                     .padding(end = dimensionResource(id = R.dimen.child_edge_padding))
             )
             TextField(
                 value = textState.value,
                 onValueChange = { textState.value = it },
-                modifier = Modifier
-                    .padding(start = dimensionResource(id = R.dimen.child_edge_padding)),
-                placeholder = { Text(text = stringResource(id = R.string.login_password_hint)) }
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = MaterialTheme.colors.onPrimary,
+                    backgroundColor = MaterialTheme.colors.background,
+                    errorIndicatorColor = MaterialTheme.colors.error
+                ),
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.child_edge_padding)),
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.login_password_hint),
+                        style = MaterialTheme.typography.body1
+                    )
+                }
             )
         }
 
@@ -144,12 +190,28 @@ fun LoginScreen() {
         ) {
             Button(
                 onClick = {  },
-                content = { Text(text = stringResource(id = R.string.login)) },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.primary
+                ),
+                content = {
+                    Text(
+                        text = stringResource(id = R.string.button_login),
+                        style = MaterialTheme.typography.button
+                    )
+                },
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.child_edge_padding))
             )
             Button(
                 onClick = {  },
-                content = { Text(text = stringResource(id = R.string.create_account)) },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.primary
+                ),
+                content = {
+                    Text(
+                        text = stringResource(id = R.string.button_create_account),
+                        style = MaterialTheme.typography.button,
+                    )
+                },
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.child_edge_padding))
             )
         }
--- app/src/main/java/com/huntergaming/classicsolitaire/ui/theme/Type.kt
+++ app/src/main/java/com/huntergaming/classicsolitaire/ui/theme/Type.kt
@@ -1,10 +1,13 @@
 package com.huntergaming.classicsolitaire.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.sp
 import com.huntergaming.classicsolitaire.R
 
@@ -24,39 +27,68 @@ private val ColombiaBold = FontFamily(
     Font(R.font.colombia, FontWeight.Bold)
 )
 
-// Set of Material typography styles to start with
-val LightTypography = Typography(
+val DarkTypography = Typography(
     h1 = TextStyle(
-        fontFamily = AntiCoronaBoldTitle
+        fontFamily = AntiCoronaBoldTitle,
+        fontSize = 34.sp,
+        letterSpacing = 2.sp,
+        shadow = Shadow(BackgroundDark, Offset(.8f, .8f), .5f),
+        color = OnDark
     ),
     h4 = TextStyle(
-        fontFamily = AntiCoronaTitle
+        fontFamily = AntiCoronaTitle,
+        fontSize = 30.sp,
+        letterSpacing = 1.5.sp,
+        shadow = Shadow(BackgroundDark, Offset(.4f, .2f), .3f),
+        color = OnDark
     ),
     h6 = TextStyle(
-        fontFamily = AntiCoronaTitle
+        fontFamily = AntiCoronaTitle,
+        fontSize = 20.sp,
+        color = OnDark
     ),
     body1 = TextStyle(
-        fontFamily = Colombia
+        fontFamily = Colombia,
+        fontSize = 18.sp,
+        color = OnDark
     ),
     button = TextStyle(
-        fontFamily = ColombiaBold
+        fontFamily = ColombiaBold,
+        fontSize = 25.sp,
+        textDecoration = TextDecoration.Underline,
+        color = OnDark
     )
 )
 
-val DarkTypography = Typography(
+val LightTypography = Typography(
     h1 = TextStyle(
-        fontFamily = AntiCoronaBoldTitle
+        fontFamily = AntiCoronaBoldTitle,
+        fontSize = 34.sp,
+        letterSpacing = 2.sp,
+        shadow = Shadow(BackgroundLight, Offset(.8f, .8f), .5f),
+        color = OnLight
     ),
     h4 = TextStyle(
-        fontFamily = AntiCoronaTitle
+        fontFamily = AntiCoronaTitle,
+        fontSize = 30.sp,
+        letterSpacing = 1.5.sp,
+        shadow = Shadow(BackgroundLight, Offset(.4f, .2f), .3f),
+        color = OnLight
     ),
     h6 = TextStyle(
-        fontFamily = AntiCoronaTitle
+        fontFamily = AntiCoronaTitle,
+        fontSize = 20.sp,
+        color = OnLight
     ),
     body1 = TextStyle(
-        fontFamily = Colombia
+        fontFamily = Colombia,
+        fontSize = 16.sp,
+        color = OnLight
     ),
     button = TextStyle(
-        fontFamily = ColombiaBold
+        fontFamily = ColombiaBold,
+        fontSize = 14.sp,
+        textDecoration = TextDecoration.Underline,
+        color = OnLight
     )
 )
